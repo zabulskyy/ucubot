709eb449cc0f2fa7db50331794e816286f5c1429
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2018-03-09 12:41:36 +0200

    Update LessonSignalDto.cs

--- ucubot/Model/LessonSignalDto.cs
+++ ucubot/Model/LessonSignalDto.cs
@@ -6,7 +6,7 @@ namespace ucubot.Model
     {
         public int Id { get; set; }
         public string UserId { get; set; }
-        public LessonSignalType SignalType { get; set; }
+        public LessonSignalType Type { get; set; }
         public DateTime Timestamp { get; set; }
     }
 }
